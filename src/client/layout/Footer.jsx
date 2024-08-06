6b4779f92de488cb95c68627b6b866f9ce657954
CBryant20 <150076515+CBryant20@users.noreply.github.com>
2024-08-06 13:45:53 -0700

    Footer created and functional

---
+++ src/client/layout/Footer.jsx
@@ -0,0 +1,20 @@
+import "./Footer.less";
+
+export default function Footer() {
+  return (
+    <nav className='icons'>
+      <div className='icon-container'>
+        <i className='ri-facebook-fill'></i>
+      </div>
+      <div className='icon-container'>
+        <i className='ri-twitter-fill'></i>
+      </div>
+      <div className='icon-container'>
+        <i className='ri-instagram-fill'></i>
+      </div>
+      <div className='icon-container'>
+        <i className='ri-linkedin-fill'></i>
+      </div>
+    </nav>
+  );
+}
