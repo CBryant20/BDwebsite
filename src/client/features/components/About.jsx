9a42d6406101a34303d5ef2246ce0b10f3c35556
CBryant20 <150076515+CBryant20@users.noreply.github.com>
2024-08-07 10:56:52 -0700

    Hero and AppHome updated

--- src/client/features/components/About.jsx
+++ src/client/features/components/About.jsx
@@ -1,4 +1,47 @@
+import { useEffect } from "react";
+import Typed from "typed.js";
+
 export default function About() {
+  useEffect(() => {
+    const webDevTyped = new Typed("#web-dev-text", {
+      strings: [
+        "Full Stack",
+        "Front-End",
+        "Back-End",
+        "Freelance",
+        "UI/UX",
+        "Mobile First",
+        "Responsive Design",
+        "Consultation",
+      ],
+      typeSpeed: 30,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    const woodWorkerTyped = new Typed("#woodworker-text", {
+      strings: [
+        "Skilled",
+        "Creative",
+        "Passionate",
+        "Crafty",
+        "Detailed",
+        "Dedicated",
+        "Precise",
+        "Talented",
+        "Resourceful",
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    });
+
+    return () => {
+      webDevTyped.destroy();
+      woodWorkerTyped.destroy();
+    };
+  }, []);
+
   return (
     <div className='columns'>
       <div className='column'>
--- src/client/features/components/Hero.jsx
+++ src/client/features/components/Hero.jsx
@@ -1,46 +1,8 @@
-import React, { useEffect } from "react";
-import Typed from "typed.js";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Hero() {
-  useEffect(() => {
-    const webDevTyped = new Typed("#web-dev-text", {
-      strings: [
-        "Full Stack",
-        "Front-End",
-        "Back-End",
-        "Freelance",
-        "UI/UX",
-        "Mobile First",
-        "Responsive Design",
-        "Consultation",
-      ],
-      typeSpeed: 30,
-      backSpeed: 30,
-      loop: true,
-    });
-
-    const woodWorkerTyped = new Typed("#woodworker-text", {
-      strings: [
-        "Skilled",
-        "Creative",
-        "Passionate",
-        "Crafty",
-        "Detailed",
-        "Dedicated",
-        "Precise",
-        "Talented",
-        "Resourceful",
-      ],
-      typeSpeed: 30,
-      backSpeed: 30,
-      loop: true,
-    });
-
-    return () => {
-      webDevTyped.destroy();
-      woodWorkerTyped.destroy();
-    };
-  }, []);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -64,13 +26,18 @@ export default function Hero() {
     };
   }, []);
 
+  const handleContactClick = () => {
+    navigate("/contact");
+  };
+
   return (
     <section className='hero'>
       <div className='hero-content'>
         <h4>WELCOME TO</h4>
         <h1>Bryant Designs</h1>
-
-        <button id='hero-btn'>CONTACT</button>
+        <button id='hero-btn' onClick={handleContactClick}>
+          CONTACT
+        </button>
       </div>
       <div className='circle-container'>
         <div className='circle c1' data-speed='5'></div>
