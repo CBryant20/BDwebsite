0a90cce09d8946eca6a6b8fd73f45c8dc988b581
CBryant20 <150076515+CBryant20@users.noreply.github.com>
2024-08-09 10:35:31 -0700

    Hero video loading created

--- src/client/features/components/Hero.jsx
+++ src/client/features/components/Hero.jsx
@@ -32,9 +32,11 @@ export default function Hero() {
 
   return (
     <section className='hero'>
+      <video className='hero-video' autoPlay muted loop playsInline>
+        <source src='src/images/BD_logo_video.mp4' type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
       <div className='hero-content'>
-        <h4>WELCOME TO</h4>
-        <h1>Bryant Designs</h1>
         <button id='hero-btn' onClick={handleContactClick}>
           CONTACT
         </button>
@@ -42,10 +44,6 @@ export default function Hero() {
       <div className='circle-container'>
         <div className='circle c1' data-speed='5'></div>
         <div className='circle c2' data-speed='7'></div>
-      </div>
-      <div className='icon-container'>
-        <i className='fas fa-hammer icon hammer'></i>
-        <i className='fas fa-laptop-code icon computer'></i>
       </div>
     </section>
   );
